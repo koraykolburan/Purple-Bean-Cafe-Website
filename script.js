254420a0eb8ca950ad605694a25cfda7059c121e
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2021-10-29 16:03:28 +0300

    First Deployment!

---
+++ script.js
@@ -0,0 +1,7 @@
+//for popover!
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
